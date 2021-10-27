51cb0b5ba8cb0261e88bf220283c8acffe27f3f0
SatrioPratama75 <satrio7299@gmail.com>
2021-10-27 15:43:29 +0700

    Luas dan Keliling

---
+++ rumuslingkaran.py
@@ -0,0 +1,10 @@
+#jarilingkaran = 8
+#luas = pi*(r*r)
+#keliling = 2*pi*r
+import math 
+r = float(input("Jari-Jarinya adalah : "))
+luas = math.pi*r*r
+keliling = 2*math.pi*r
+
+print("Luas Lingkaran = ", format(luas,'.2f'))
+print("Keliling Lingkaran = ", format(keliling,'.2f'))
